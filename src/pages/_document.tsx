a95d022c12dee13fc4bbfa608643e54c1d647bc4
Tassio Rego <tassiorego@gmail.com>
2020-09-10 00:35:34 -0300

    fix: head import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,10 @@ import Document, {
   DocumentContext,
   Html,
   Main,
+  Head,
   NextScript,
 } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import Head from 'next/head';
 
 export default class MyDocument extends Document {
   static async getInitialProps(
